f65e193c50d007ec7631c3e2f1f8d495117efc5d
mbb <asu_mbb@tfoms.local>
2017-04-07 13:48:31 +0300

    PEP8 requirements

--- fb2rn.py
+++ fb2rn.py
@@ -19,14 +19,16 @@ import os
 import sys
 from lxml import etree
 import glob
-#import re
+import traceback
+
+# import re
 
 __AUTHOR__ = 'MBB'
 __VERSION__ = 0.03
-__all__ = ['stripBetween', 'getFB2tags', 'getFB2tagsXML']
+__all__ = ['strip_between', 'get_fb2_tags', 'get_fb2_tags_xml']
 
 
-def stripBetween(s, sb='', se=''):
+def strip_between(s, sb='', se=''):
     """Return substring from s between sb(begin) and se(end) substrings
        DEPRECATED
     """
@@ -36,28 +38,27 @@ def stripBetween(s, sb='', se=''):
     return tmp.partition(se)[0]
 
 
-def getFB2tags(fb2):
+def get_fb2_tags(fb2):
     """From FB2-file header retrieve author and title and sequence tags
        DEPRECATED
     """
-    f_name = l_name = b_title = seq_name = seq_n = ''
     f = open(fb2, encoding='utf-8')
     try:
         line = f.read(500)
     except UnicodeDecodeError:
-        f.close
+        f.close()
         f = open(fb2, encoding='windows-1251')
         line = f.read(500)
-    f.close
-    f_name = stripBetween(line, '<first-name>', '</first-name>')
-    l_name = stripBetween(line, '<last-name>', '</last-name>')
-    b_title = stripBetween(line, '<book-title>', '</book-title>')
-    seq_name = stripBetween(line, '<sequence name="', 'number')[:-2]
-    seq_n = stripBetween(line, 'number="', '"')
-    return f_name, l_name, b_title, seq_name, seq_n
+    f.close()
+    f_n = strip_between(line, '<first-name>', '</first-name>')
+    l_n = strip_between(line, '<last-name>', '</last-name>')
+    b_t = strip_between(line, '<book-title>', '</book-title>')
+    s_name = strip_between(line, '<sequence name="', 'number')[:-2]
+    s_n = strip_between(line, 'number="', '"')
+    return f_n, l_n, b_t, s_name, s_n
 
 
-def getFB2tagsXML(fb2):
+def get_fb2_tags_xml(fb2):
     """From FB2-file header retrieve author and title and sequence tags
     """
     ft = '{http://www.gribuser.ru/xml/fictionbook/2.0}first-name'
@@ -65,25 +66,25 @@ def getFB2tagsXML(fb2):
     bt = '{http://www.gribuser.ru/xml/fictionbook/2.0}book-title'
     sq = '{http://www.gribuser.ru/xml/fictionbook/2.0}sequence'
     ti = '{http://www.gribuser.ru/xml/fictionbook/2.0}title-info'
-    f_name = l_name = b_title = seq_name = seq_n = ''
+    f_n = l_n = b_t = s_name = s_n = ''
     c = etree.iterparse(fb2, events=('end',), tag=[ft, lt, bt, sq, ti])
     for event, elem in c:
-        if not f_name and elem.tag == ft:
-            f_name = elem.text
-        if not l_name and elem.tag == lt:
-            l_name = elem.text
-        if not b_title and elem.tag == bt:
-            b_title = elem.text
-        if not seq_name and elem.tag == sq:
-            seq_name = elem.get('name')
-            seq_n = elem.get('number')
+        if not f_n and elem.tag == ft:
+            f_n = elem.text
+        if not l_n and elem.tag == lt:
+            l_n = elem.text
+        if not b_t and elem.tag == bt:
+            b_t = elem.text
+        if not s_name and elem.tag == sq:
+            s_name = elem.get('name')
+            s_n = elem.get('number')
         elem.clear()
         while elem.getprevious() is not None:
             del elem.getparent()[0]
         if elem.text == 'title-info':
             break
     del c
-    return f_name, l_name, b_title, seq_name, seq_n
+    return f_n, l_n, b_t, s_name, s_n
 
 
 if __name__ == '__main__':
@@ -94,24 +95,25 @@ if __name__ == '__main__':
         print('1st argument is bad!')
         exit(1)
 
-#    _bad = """[!@#$&~%\*\(\)\[\]\{\}"'\\:;><`]"""
-#    print(ar_1st, re.search(_bad, ar_1st))
-    _bad = """!@#$&~%*()[]{}"'\:;><`"""
+    #    _bad = """[!@#$&~%\*\(\)\[\]\{\}"'\\:;><`]"""
+    #    print(ar_1st, re.search(_bad, ar_1st))
+
     for _c in ar_1st:
-        if _c in _bad:
+        if _c in """!@#$&~%*()[]{}"'\:;><`""":
             print(_c, 'is bad separator!')
             exit(2)
 
     for ar1 in sys.argv[2:]:
         for ar in glob.glob(ar1):
             if os.path.exists(ar):
-                f_name, l_name, b_title, seq_name, seq_n = getFB2tagsXML(ar)
+                f_name, l_name, b_title, seq_name, seq_n = get_fb2_tags_xml(ar)
                 d = dict(F=f_name.upper(), f=f_name, L=l_name.upper(), l=l_name, T=b_title.upper(), t=b_title,
                          S=seq_name.upper(), s=seq_name, N=seq_n, n=seq_n)
                 newFB2 = ''.join(d.get(c, c) + (' ' if _1st == '+' else '') for c in ar_1st)
                 try:
                     os.rename(ar, newFB2.rstrip() + '.fb2')
-                except:
+                except OSError:
                     print('I can`t to rename', ar)
+                    print(traceback.print_exc())
             else:
                 print("File", ar, "not found!")
